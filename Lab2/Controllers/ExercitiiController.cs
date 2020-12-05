615489ff29bef90b24ce79888aee1a766240f97a
achimelenaluiza <elena-luiza.achim@hpe.com>
2020-12-05 16:20:53 +0200

    Lab2 -ex1,2

---
+++ Lab2/Controllers/ExercitiiController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ExercitiiController : Controller
+    {
+        // GET: Exercitii
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Exercitiul 1:
+        // https://localhost:44376/cautare/ex1/bunaziua/buna 
+
+        public ContentResult CautareSubstring(string cuvant, string propozitie)
+        {
+            string mesaj = "Propozitia '" + propozitie + "' contine cuvantul '" + cuvant + "' !";
+            string error1 = "Propozitia '" + propozitie + "'NU contine cuvantul '" + cuvant + "' !";
+            string error2 = "Lipseste un parametru";
+
+            if (propozitie != null && cuvant != null)
+            {
+                if (propozitie.Contains(cuvant))
+                    return Content(mesaj);
+                return Content(error1);
+            }
+            return Content(error2);
+        }
+        // Exercitiul 2:
+        // https://localhost:44376/cautare/ex2/buna/sal
+        public ActionResult CautareSubstringOptional(string cuvant, string? propozitie)
+        {
+            ViewBag.message = "Propozitia `" + propozitie + "` contine cuvantul `" + cuvant + "`";
+
+            if (propozitie == null)
+            {
+                return HttpNotFound("Parametrul propozitie lipseste!");
+            }
+            if (!propozitie.Contains(cuvant))
+            {
+                ViewBag.message = "Propozitia `" + propozitie + "` NU contine cuvantul `" + cuvant + "`";
+            }
+            return View();
+        }
+    }
+    
+}
---
+++ Proiect/Models/Figura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proiect.Models
+{
+    public class Figura
+    {
+        private string nume;
+        public string Nume
+        {
+            get {return nume;}
+            set {nume = value;}
+        }
+    }
+}
